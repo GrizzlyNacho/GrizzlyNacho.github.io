5c81c0c93a338081a60c1b8819608836c344cc57
Nathan <nlutz91@gmail.com>
2017-07-15 23:24:34 -0400

    generate resume

--- py/generate_resume.py
+++ py/generate_resume.py
@@ -1,8 +1,13 @@
 import pystache
+import json
 from py import util
 
 def generateResume():
 	tmplPage = util.loadFile("templates/page.html")
 	tmplContents = util.loadFile("templates/contents_resume.html")
-	generated = pystache.render(tmplPage, {'contents': tmplContents})
+	defResume = json.loads(util.loadFile("definitions/resume.json"))
+
+	genContents = pystache.render(tmplContents, defResume)
+	generated = pystache.render(tmplPage, {'contents': genContents})
+	
 	return generated
