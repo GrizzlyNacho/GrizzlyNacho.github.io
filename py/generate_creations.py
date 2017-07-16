0436bc7c46ef78cfb3c45bade7fb1b9efaf7aebe
Nathan <nlutz91@gmail.com>
2017-07-16 17:53:10 -0400

    add templating for creations

--- py/generate_creations.py
+++ py/generate_creations.py
@@ -5,9 +5,9 @@ from py import util
 def generateCreations():
 	tmplPage = util.loadFile("templates/page.html")
 	tmplContents = util.loadFile("templates/contents_creations.html")
-	#defResume = json.loads(util.loadFile("definitions/creations.json"))
+	defCreations = json.loads(util.loadFile("definitions/creations.json"))
 
-	#genContents = pystache.render(tmplContents, defResume)
-	generated = pystache.render(tmplPage, {'contents': tmplContents})
+	genContents = pystache.render(tmplContents, defCreations)
+	generated = pystache.render(tmplPage, {'contents': genContents})
 	
 	return generated
