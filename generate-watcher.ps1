f16e2a6cf07a1adc8cd361e1250fe555540f3efd
Nathan <nlutz91@gmail.com>
2018-05-27 20:54:21 -0400

    styled resume

---
+++ generate-watcher.ps1
@@ -0,0 +1,16 @@
+### SET FOLDER TO WATCH + FILES TO WATCH + SUBFOLDERS YES/NO
+    $watcher = New-Object System.IO.FileSystemWatcher
+    $watcher.Path = "C:\Users\Nathan\Documents\GitHub\GrizzlyNacho.github.io"
+    $watcher.Filter = "*.html"
+    $watcher.IncludeSubdirectories = $true
+    $watcher.EnableRaisingEvents = $true  
+
+### DEFINE ACTIONS AFTER AN EVENT IS DETECTED
+    $action = { py C:\Users\Nathan\Documents\GitHub\GrizzlyNacho.github.io\generateHtml.py
+              }    
+### DECIDE WHICH EVENTS SHOULD BE WATCHED 
+    Register-ObjectEvent $watcher "Created" -Action $action
+    Register-ObjectEvent $watcher "Changed" -Action $action
+    Register-ObjectEvent $watcher "Deleted" -Action $action
+    Register-ObjectEvent $watcher "Renamed" -Action $action
+    while ($true) {sleep 5}
